8880f72cf93cb66f71ed487fefc93773967adebc
wins <win4709hi@gmail.com>
2022-08-28 21:38:33 +0800

    chore: deploy setting

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+# 发生错误时终止
+set -e
+
+# 构建
+yarn build
+
+# 进入构建文件夹
+cd dist
+
+# 如果你要部署到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# 如果你要部署在 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:winszhuang/hex_react_todolist.git main:gh-pages
+
+cd -
